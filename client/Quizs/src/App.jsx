838745693569ca3bee500e1570a117b172c85e9c
HiMashuguptA <sg707012@gmail.com>
2024-04-12 22:56:26 +0530

    Added file structure

---
+++ client/Quizs/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Homepage from './components/Homepage'
+
+function App() {
+  return (
+    <>
+      <Homepage/>
+    </>
+  )
+}
+
+export default App
