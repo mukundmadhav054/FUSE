ec8e361dd5fda1625b50db6eb4fa975b63ae0bb8
mukundmadhav054 <mukundmadhav054@gmail.com>
2024-04-13 10:20:07 +0530

    created dsa course page, added route

--- client/Quizs/src/App.jsx
+++ client/Quizs/src/App.jsx
@@ -1,23 +1,24 @@
-import { useState } from 'react'
-import './App.css'
-import Homepage from './components/Homepage'
-import Signup from './components/Authentication/Signup'
-import {Routes,Route} from "react-router-dom";
-import Login from './components/Authentication/Login';
-import CoursePage from './components/CoursePage';
+import { useState } from "react";
+import "./App.css";
+import Homepage from "./components/Homepage";
+import Signup from "./components/Authentication/Signup";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Authentication/Login";
+import CoursePage from "./components/CoursePage";
+import Dsacourse from "./components/courses/Dsacourse";
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path='/' element={<Homepage/>}/>
-      <Route path='/courses' element={<CoursePage/>}/>
-    <Route path='/login' element={<Login />} />
-   <Route path='/signup' element={<Signup />} />
-    </Routes>
-    
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/courses" element={<CoursePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dsacourse" element={<Dsacourse />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/Quizs/src/components/courses/Dsacourse.jsx
@@ -0,0 +1,126 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+} from "@chakra-ui/react";
+const Dsacourse = () => {
+  const [data, setData] = useState([]);
+  const getData = async () => {
+    const res = await axios
+      .get("http://localhost:3000/dsa-topics")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <>
+      <Accordion allowToggle>
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box as="span" flex="1" textAlign="left">
+                Easy questions
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            {data
+              .filter((e) => e.level == "easy")
+              .map((item) => (
+                <div>
+                  <h2>{item.name}</h2>
+                  <p>{item.description}</p>
+                  <div>
+                    <iframe
+                      width="560"
+                      height="315"
+                      src={`https://www.youtube.com/embed/${item.videoEmbed}?si=5eR9XrYt_vIu1ob1`}
+                      title="YouTube video player"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                    ></iframe>
+                  </div>
+                </div>
+              ))}
+          </AccordionPanel>
+        </AccordionItem>
+
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box as="span" flex="1" textAlign="left">
+                Moderate questions
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            {data
+              .filter((e) => e.level == "medium")
+              .map((item) => (
+                <div>
+                  <h2>{item.name}</h2>
+                  <p>{item.description}</p>
+                  <div>
+                    <iframe
+                      width="560"
+                      height="315"
+                      src={`https://www.youtube.com/embed/${item.videoEmbed}?si=5eR9XrYt_vIu1ob1`}
+                      title="YouTube video player"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                    ></iframe>
+                  </div>
+                </div>
+              ))}
+          </AccordionPanel>
+        </AccordionItem>
+
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box as="span" flex="1" textAlign="left">
+                Hard questions
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            {data
+              .filter((e) => e.level == "hard")
+              .map((item) => (
+                <div>
+                  <h2>{item.name}</h2>
+                  <p>{item.description}</p>
+                  <div>
+                    <iframe
+                      width="560"
+                      height="315"
+                      src={`https://www.youtube.com/embed/${item.videoEmbed}?si=5eR9XrYt_vIu1ob1`}
+                      title="YouTube video player"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                    ></iframe>
+                  </div>
+                </div>
+              ))}
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
+    </>
+  );
+};
+
+export default Dsacourse;
